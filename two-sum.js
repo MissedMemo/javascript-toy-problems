ee42c6a5cbaca56aa64e2dc2b66432c0061c3195
AndyL <missedmemo@gmail.com>
2019-05-17 14:13:09 -0700

    define problem statement

---
+++ two-sum.js
@@ -0,0 +1,9 @@
+/****************************************************************************************
+ (from leetcode) : for an array of integers, return indices of the two elements whose
+ sum equals the specified target.
+ 
+ e.g. array [2, 7, 11, 15] with a target of 9 resolves to index values 0 & 1 (2 + 7 = 9)
+ 
+ ***************************************************************************************/
+
+ 
