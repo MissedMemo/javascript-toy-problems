b906f850ff62eb4a6c8d4d2ea6c0d98ccedb42a7
AndyL <missedmemo@gmail.com>
2019-05-17 14:53:52 -0700

    solved!

--- two-sum.js
+++ two-sum.js
@@ -6,4 +6,32 @@
  
  ***************************************************************************************/
 
- 
+/*
+const twoSum = (nums, target) => {
+
+  const results = []
+
+  for( let i = 0; i < nums.length; i++ ) {
+    for( let i2 = 0; i2 < nums.length; i2++ ) {
+      const n = nums[i]
+      const n2 = nums[i2]
+      if ( n + n2 === target ) {
+        return [i, i2]
+      }
+    }
+  }
+};
+*/
+
+const twoSum = (nums, target) => nums.reduce( (indices, nCurrent, i) => {
+  if ( nums.find( n => n + nCurrent === target) ) {
+    indices.push(i)
+  }
+  return indices
+}, [] );
+
+console.log( 'indices for twoSum([2,7,11,15], 9): %o (should be [0,1])', twoSum([2,7,11,15], 9) )
+console.log( 'indices for twoSum([2,7,11,15], 13): %o (should be [0,2])', twoSum([2,7,11,15], 13) )
+console.log( 'indices for twoSum([2,7,11,15], 13): %o (should be [0,3])', twoSum([2,7,11,15], 17) )
+console.log( 'indices for twoSum([2,7,11,15], 18): %o (should be [1,2])', twoSum([2,7,11,15], 18) )
+console.log( 'indices for twoSum([2,7,11,15], 26): %o (should be [2,3])', twoSum([2,7,11,15], 26) )
